05908780b52b7ae8c78d95a69724f74b03833e42
GYoung <ytok1108@aol.com>
2026-03-24 21:22:01 +0900

    struct impl

---
+++ 001_study_glibc/training_c/01_String_Tsoding/01_Rust_Code/a02_string_view_impl/src/main.rs
@@ -0,0 +1,36 @@
+#[derive(Debug, Clone, Copy)]
+struct StringView<'a> {
+    data: &'a str,
+    count: usize,
+}
+
+impl<'a> StringView<'a> {
+    fn sv(s: &str) -> StringView<'_> {
+        StringView {
+            data: s,
+            count: s.len(),
+        }
+    }
+
+    fn sv_chop_left(sv: &mut StringView, n: usize) {
+        let n = n.min(sv.count);
+
+        sv.data = &sv.data[n..];
+        sv.count -= n;
+    }
+    fn sv_chop_right(sv: &mut StringView, n: usize) {
+        let n = n.min(sv.count);
+
+        sv.count -= n;
+        sv.data = &sv.data[..sv.count];
+    }
+}
+
+fn main() {
+    let mut s = StringView::sv("Hello, World");
+
+    StringView::sv_chop_right(&mut s, 3);
+    StringView::sv_chop_left(&mut s, 2);
+
+    println!("{}", s.data);
+}
